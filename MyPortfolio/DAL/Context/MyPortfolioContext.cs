c2f1789ef2eb54d1c428f09f84050c78484505d6
syrupoffical <batuhanozdemir0222@gmail.com>
2024-06-04 00:06:58 +0300

    04.06.2024 00:06

---
+++ MyPortfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=BATUHANO-NB\\MSSQLSERVER2022;Initial Catalog=MyPortfolioDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ MyPortfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string? FeatureDescription { get; set; }
+        public string? Description { get; set; }
+    }
+}
